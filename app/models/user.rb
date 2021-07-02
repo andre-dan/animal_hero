ef481ff627c0bf95b22e3c441d9fd6f2a4d4e83b
andre-dan <andrerndc@gmail.com>
2021-07-01 22:09:45 -0300

    Vendo somente pets das ongs cadastradas pelo usuario

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_secure_password
   before_save :email_downcase
   has_many :ongs
+  has_many :pets, through: :ongs
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
